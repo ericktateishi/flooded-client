fb8cd0b36702dd210f4d826b128c1e225ced40ee
Erick <erick.jesus@startse.com>
2020-09-28 23:20:28 -0300

    fixed untracked files

--- src/app/components/Post/Loading.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import Skeleton from '@material-ui/lab/Skeleton'
-
-const Loading = () => [0,1,2,3,4,5].map(i => 
-  <Skeleton key={i} variant="rect" width={'100%'} height={140} style={{marginBottom: 10}}/>
-)
-
-export default Loading
--- src/app/styled/AppTitle.js
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components'
-
-export default styled.h2`
-  width: 100%;
-  font-weight: 100;
-`
